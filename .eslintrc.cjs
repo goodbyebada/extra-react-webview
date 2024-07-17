414f720df196f159f92a5b16a74e532cf83ce3ed
geon-2 <gunle0416@gmail.com>
2024-07-17 14:48:18 +0900

    Chore: Install husky and set eslint, prettier format rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,26 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
+    'react/react-in-jsxx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-}
+};
