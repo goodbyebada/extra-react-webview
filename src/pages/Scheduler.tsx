44ae687f00d0f55cf4ad823a34a3df05867651ff
goodbyebada <goodbyebada@naver.com>
2024-08-07 14:29:26 +0900

    Rename : import문 경로 modal 대소문자 수정

--- src/pages/Scheduler.tsx
+++ src/pages/Scheduler.tsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 import { useState } from "react";
 import useCalendar from "@utills/useCalendar";
 import DateSelectorItem from "@components/DateSelectorItem";
-import ScheduleModal from "@components/modal/ScheduleModal";
+import ScheduleModal from "@components/Modal/ScheduleModal";
 
 /**
  *
