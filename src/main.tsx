e666ff20e18707f0f2b9fde2cb8d180435377ddc
goodbyebada <goodbyebada@naver.com>
2024-08-08 18:27:43 +0900

    Fix: rebase  중 오류 fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "./App.tsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-=======
 import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
@@ -18,6 +10,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <App />
     </Provider>
->>>>>>> d4b43e5e1bad0fbc529e1c9004daa172a732b614
   </React.StrictMode>,
 );
