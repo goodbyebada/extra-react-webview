32f5882bcbed590a59f59fb5e85ab87b97449d58
geon-2 <gunle0416@gmail.com>
2024-12-25 22:54:04 +0900

    Chore: Set https options 모바일 카메라 테스트를 위한 https 호스팅 허용 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    https: {
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
+    },
+  },
 });
 
 // 절대경로 참고용
