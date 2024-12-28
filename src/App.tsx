689892d312fe40f1b14db232442c161d7770b62d
goodbyebada <goodbyebada@naver.com>
2024-12-28 18:38:30 +0900

    fix: labelCheckBox bug 수정
    - 처음 label 선택시 초기화 되는 bug 수정
    - 이전 사용하던 레거시 코드 App route 제거

--- src/App.tsx
+++ src/App.tsx
@@ -152,8 +152,6 @@ function App() {
             element={<ManagerDashboard />}
           />
 
-          <Route path="/manager-dashboard" element={<ManagerDashboard />} />
-
           {/* 업체 측 공고 화면*/}
           <Route path="/job-post-management" element={<JobPostManagement />} />
 
