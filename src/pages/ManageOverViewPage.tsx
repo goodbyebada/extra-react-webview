0b8e38257477bd74760d1e5477bb8f7f34e3a873
goodbyebada <goodbyebada@naver.com>
2024-12-31 18:23:42 +0900

    Refactor: 업체 측 현장관리 촬영목록 화면 페이지 추가
    - 더미데이터로 임시 구현 -> 추후 mock API 연결해 수정 예정
    
    - fixed nav bar 쓰는 컴포넌트를 위해 ContentWrapper 추가해 space 설정할 수 있게 하였음

---
+++ src/pages/ManageOverViewPage.tsx
@@ -0,0 +1,54 @@
+/**
+ * 업체 측 현장관리
+ * 촬영목록 화면
+ */
+
+import { CompanyNavBar } from "@components/mocules/navBar/CompanyNavBar";
+import Text from "@components/atoms/Text";
+import { ContentWrapper, LineWrapper } from "@components/atoms/Wrapper";
+import { memberRoleFrontDummyData } from "@api/dummyData";
+import Item from "@components/mocules/Item";
+import styled from "styled-components";
+
+// TODO dummyData로 구현되어있음 추후 API로 수정 예정
+
+export default function ManageOverViewPage() {
+  return (
+    <>
+      <LineWrapper>
+        <CompanyNavBar>
+          <Text size={25} weight={900}>
+            현장 관리
+          </Text>
+        </CompanyNavBar>
+      </LineWrapper>
+      <ContentWrapper marginTop="2rem" paddingLeft="10px" paddingRight="10px">
+        <Text weight={800}>촬영 관리</Text>
+
+        <ItemWrapper>
+          {memberRoleFrontDummyData.map((elem) => (
+            <Item
+              title={elem.title}
+              category={elem.category}
+              date={elem.gatheringTime.split("T")[0]}
+              dDay={"D-3"}
+              company={elem.companyName}
+              time={elem.gatheringTime}
+              location={elem.gatheringLocation}
+              status={"applied"}
+              onClick={() => {}}
+            />
+          ))}
+        </ItemWrapper>
+      </ContentWrapper>
+    </>
+  );
+}
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding-top: 10px;
+`;
