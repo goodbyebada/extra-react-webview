028ad1e446587fc4c246d7beb9fb981aadeb9c05
goodbyebada <goodbyebada@naver.com>
2025-01-10 17:21:09 +0900

    fix: FCM 알림 중복 발송
    notification 객체 포함 시 자동으로 FCM push 알림 발생으로 중복 알림 발생 해결

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,6 +19,15 @@ const firebaseConfig = {
   measurementId: "G-WBFQPPHVHP",
 };
 
+class CustomPushEvent extends Event {
+  constructor(data) {
+    super("push");
+
+    Object.assign(this, data);
+    this.custom = true;
+  }
+}
+
 // 서비스워커
 self.addEventListener("install", function (e) {
   console.log("fcm sw install..");
@@ -29,51 +38,101 @@ self.addEventListener("activate", function (e) {
   console.log("fcm sw activate..");
 });
 
-// sw.js
-self.addEventListener("push", function (event) {
-  // console.log(event.data.text());
-  const json = event.data.json();
-  const data = json.notification;
-  const title = data.title;
+/*
+ * Overrides push notification data, to avoid having 'notification' key and firebase blocking
+ * the message handler from being called
+ */
+self.addEventListener("push", (e) => {
+  // Skip if event is our own custom event
+  if (e.custom) return;
+
+  // Kep old event data to override
+  const oldData = e.data;
+
+  // Create a new event to dispatch, pull values from notification key and put it in data key,
+  // and then remove notification key
+  const newEvent = new CustomPushEvent({
+    data: {
+      legacy: oldData.json(),
+      json() {
+        const newData = oldData.json();
+        newData.data = {
+          ...newData.data,
+          ...newData.notification,
+        };
+        delete newData.notification;
+        return newData;
+      },
+    },
+    waitUntil: e.waitUntil.bind(e),
+  });
+
+  // Stop event propagation
+  e.stopImmediatePropagation();
+
+  // Dispatch the new wrapped event
+  dispatchEvent(newEvent);
+
+  const pushData = newEvent.data.json().data;
+  const title = pushData.title;
   const options = {
-    body: data.body,
-    icon: data.image,
-    badge: data.image,
+    body: pushData.body,
+    icon: pushData.image,
+    badge: pushData.image,
   };
 
-  event.waitUntil(self.registration.showNotification(title, options));
+  console.log(pushData);
+  console.log(title);
+  console.log(options);
+
+  newEvent.waitUntil(self.registration.showNotification(title, options));
 });
+
+// sw.js
+// self.addEventListener("push", function (event) {
+//   console.log(event.data.text());
+//   console.log("실행!");
+//   // const json = event.data.json();
+//   // const data = json.notification;
+//   // const title = data.title;
+//   // const options = {
+//   //   body: data.body,
+//   //   icon: data.image,
+//   //   badge: data.image,
+//   // };
+//   // event.waitUntil(self.registration.showNotification(title, options));
+//   // These are handled by the FCM SDK automatically.
+//   // webAppShowNotificaiton("push Message");
+// });
 
 //  웹 앱 포그라운드, 백그라운드 알림
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // TODO PWA 환경 설정 후 수정예정
-function webAppShowNotificaiton() {
+function webAppShowNotificaiton(tmpBody) {
   // 임시
-  const notificationTitle = "Background Message Title";
+  const notificationTitle = tmpBody + "title";
   const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
+    body: tmpBody,
+    icon: "/pwa-64x64.png",
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 }
 
 messaging.onMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received Foreground message ",
-    payload,
-  );
-  webAppShowNotificaiton();
+  console.log("포어그라운드 ", payload);
+
+  const tmpBody = "포어그라운드 ";
+  webAppShowNotificaiton(tmpBody);
 });
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload,
-  );
-  webAppShowNotificaiton();
+  console.log("백그라운드", payload);
+
+  const tmpBody = "백그라운드";
+  webAppShowNotificaiton(tmpBody);
 });
 
 self.addEventListener("notificationclick", (event) => {
