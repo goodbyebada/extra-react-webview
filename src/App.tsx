cf03d2784f4f78a5f439565d42dc8804e7bb61a2
nohyewon <yw4913@naver.com>
2025-01-28 03:35:31 +0900

    Feat: Kakaomap 지도 구현

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import MemberClothesConfirm from "@pages/MemberClothesConfirm";
 import CompanyClothesApproval from "@pages/CompanyClothesApproval";
 import CompanyClothesConfirm from "@pages/CompanyClothesConfirm";
 import MemberClothesComment from "@pages/MemberClothesComment";
+import KakaoMap from "@pages/KakaoMap";
 
 import ThemePreviewPage from "@pages/ThemePreview";
 
@@ -89,6 +90,7 @@ function App() {
             path="/company/clothes-confirm"
             element={<CompanyClothesConfirm />}
           />
+          <Route path="/member/kakaomap" element={<KakaoMap />} />
 
           <Route path="/*" element={<NotFound />} />
 
