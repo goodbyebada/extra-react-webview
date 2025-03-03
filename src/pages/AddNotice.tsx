2019e755f51604e6ebc9fa037fa3c8cde87fb1d7
goodbyebada <goodbyebada@naver.com>
2025-03-03 19:26:07 +0900

    Fix: rebase하며 잘못된 오류 해결
    - path fix
    - type Place 잘못된 선언 Place로 정정

--- src/pages/AddNotice.tsx
+++ src/pages/AddNotice.tsx
@@ -10,14 +10,12 @@ import {
   SeasonEnum,
   type CategoryEnum,
   type RoleRegister,
+  Place,
 } from "@/types/shared";
 import { requestPostFetch } from "@api/utils";
 import { useNavigate } from "react-router-dom";
 import { IoCaretBackOutline } from "react-icons/io5";
-import NoticeRole from "@components/mocules/company/Notic
-
-// TODO TYPE PLACE 어딨음?
-import { type Place } from "@api/interface";
+import NoticeRole from "@components/mocules/company/NoticeRole";
 
 /**
  * AddNotice : 업체 - 공고 등록 화면
